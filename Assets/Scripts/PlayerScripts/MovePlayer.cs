a0aa1c01eafd4a001af289a1229cdcddd32d45b4
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-01-04 11:23:23 -0600

    Move script and Keep player in scene script
    player moves with arrow keys and cannot move outside of the camera bounds

---
+++ Assets/Scripts/PlayerScripts/MovePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private string horizontalAxisName;
+
+    [SerializeField]
+    private float speed;
+
+    private float horizontalAxisValue;
+	
+	void Update ()
+    {
+        GetInput();
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        horizontalAxisValue = Input.GetAxis(horizontalAxisName);
+    }
+
+    private void Move()
+    {
+        transform.Translate(horizontalAxisValue * speed * Time.deltaTime, 0, 0);//move the player along the x axis
+    }
+
+}
