2af0660f9bf1c2a62b266a6e27ab5c46fa59f726
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-01-04 12:30:27 -0600

    Things randomly spawn
    -Marshmallows, candy canes, and nasty things spawn randomly
    -editable probability
    -Spawn at a fixed rate but at a random position and move down

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed;//speed is given via the spawn script;
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(0, -1 * speed * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+    public enum ItemType
+    {
+        marshmallow, 
+        candycane,
+        nasty
+    }
+
+    #region SerializeFields
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float timeBetweenSpawning;
+
+    [SerializeField]
+    [Tooltip("The minimum time that can happen between 2 things spawning")]
+    private float minTimeBetweenSpawning;
+
+    [SerializeField]
+    [Tooltip("The multiplier for rate at which the spawns increase")]
+    private float spawnIncreaseSpeed;
+
+    [SerializeField]
+    [Tooltip("A number out of 100 that matches the percentage of that item being picked")]
+    private int candyCaneProbability, nastyProbability;
+
+    [SerializeField]
+    private GameObject marshmallowPrefab, candycanePrefab, nastyPrefab;
+    #endregion
+
+
+
+    void Start ()
+    {
+        StartCoroutine(SpawnLoop());
+	}
+	
+	private IEnumerator SpawnLoop()
+    {
+        for (; ; )
+        {
+            yield return new WaitForSeconds(timeBetweenSpawning);
+            Spawn(ChooseItemToSpawn());//spawns an item
+        }
+    }
+
+    private ItemType ChooseItemToSpawn()
+    {
+        int randomNumber = Random.Range(1, 101);//gives us a random number between 1 and 100
+
+        if (randomNumber <= candyCaneProbability)//the smallest number. If it's that number or smaller, give us a candy cane
+        {
+            return ItemType.candycane;
+        }
+        else if(randomNumber <= nastyProbability)//if it's not smaller than our candy cane,  but smaller than our nasty, make it a nasty
+        {
+            return ItemType.nasty;
+        }
+        else//otherwise we get a marshmallow
+        {
+            return ItemType.marshmallow;
+        }
+    }
+    private void Spawn(ItemType itemToSpawn)
+    {
+        GameObject instance;
+
+        if (itemToSpawn == ItemType.candycane)
+        {
+            instance = Instantiate(candycanePrefab, ChooseSpawnPos(), Quaternion.identity);
+            instance.GetComponent<MoveObject>().speed = moveSpeed * 1.5f;//these move a bit faster
+        }
+        else if (itemToSpawn == ItemType.nasty)
+        {
+            instance = Instantiate(nastyPrefab, ChooseSpawnPos(), Quaternion.identity);
+            instance.GetComponent<MoveObject>().speed = moveSpeed;
+        }
+        else if (itemToSpawn == ItemType.marshmallow)
+        {
+            instance = Instantiate(marshmallowPrefab, ChooseSpawnPos(), Quaternion.identity);
+            instance.GetComponent<MoveObject>().speed = moveSpeed;
+        }
+
+    }
+
+    private Vector2 ChooseSpawnPos()
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector2(Random.Range(0, Screen.width), Screen.height));
+    }
+}
